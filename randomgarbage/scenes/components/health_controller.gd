06ca0df9d87fc6498b2ca8e8ee51991bff68eef8
gabeee62 <96806700+gabeee62@users.noreply.github.com>
2024-01-18 10:50:43 -0500

    Add files via upload

---
+++ randomgarbage/scenes/components/health_controller.gd
@@ -0,0 +1,24 @@
+extends Node
+
+signal dead()
+
+@export var max_health: int
+var current_health: int = max_health
+
+
+func _process(_delta):
+	if current_health <= 0:
+		death()
+
+
+func heal(heal_amount):
+	damage(-heal_amount)
+
+
+func damage(dmg):
+	var previous_health = current_health
+	current_health -= dmg
+
+
+func death():
+	dead.emit()
